3f79e12249a1dea3efbcb9e335fdae2f30c6ad3a
Kastuks <71520231+Kastuks@users.noreply.github.com>
2021-09-17 16:14:45 +0300

    Update PasswordCheckerTests.java
    Uploaded changes from PC to git.

--- libraryDesign/tests/design/PasswordCheckerTests.java
+++ libraryDesign/tests/design/PasswordCheckerTests.java
@@ -15,22 +15,52 @@ class PasswordCheckerTests {
 	void setUp() {
 		passwordChecker = new PasswordChecker();
 	}
-
+	
+	
 	@Test
-	void testPasswordLength() {
-		boolean isValid = passwordChecker.validatePasswordLength(10, "qr12qwtr"); 
+	void testPasswordIsNull_shouldReturnFalse() {
+		boolean isValid = passwordChecker.validatePasswordIsNull(null);
 		assertTrue(isValid);
 	}
 	
 	@Test
-	void testPasswordUpperCaseLetters() {
+	void testPasswordIsEmpty_shouldReturnFalse() {
+		boolean isValid = passwordChecker.validatePasswordIsEmpty("");
+		assertTrue(isValid);
+	}
+	@Test
+	void testPasswordLength_shouldReturnFalse() {
+		boolean isValid = passwordChecker.validatePasswordLength(10, "qsdadsxsar12qwtr"); 
+		assertTrue(isValid);
+	}
+	@Test
+	void testPasswordLength_shouldReturnTrue() {
+		boolean isValid = passwordChecker.validatePasswordLength(10, "qr12qwtr"); 
+		assertTrue(isValid);
+	}
+	
+	
+	@Test
+	void testPasswordUpperCaseLetters_shouldReturnFalse() {
 		boolean isValid = passwordChecker.validatePasswordUpperCaseLetters("qr12qwtr");
 		assertTrue(isValid);
 	}
 	
 	@Test
-	void testPasswordSpecialSymbols() {
+	void testPasswordUpperCaseLetters_shouldReturnTrue() {
+		boolean isValid = passwordChecker.validatePasswordUpperCaseLetters("Qr12qwtr");
+		assertTrue(isValid);
+	}
+	
+	@Test
+	void testPasswordSpecialSymbols_shouldReturnFalse() {
 		boolean isValid = passwordChecker.validatePasswordSpecialSymbols("qr12qwtr");
+		assertTrue(isValid);
+	}
+	
+	@Test
+	void testPasswordSpecialSymbols_shouldReturnTrue() {
+		boolean isValid = passwordChecker.validatePasswordSpecialSymbols("q.r12qwtr");
 		assertTrue(isValid);
 	}
 }
